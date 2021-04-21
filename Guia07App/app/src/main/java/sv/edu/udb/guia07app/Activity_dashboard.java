9ab79bf127e9c1207602d2e647f3efa2b8ef861b
josselynVal <69021826+josselynVal@users.noreply.github.com>
2021-04-21 16:24:42 -0600

    Guia 7

---
+++ Guia07App/app/src/main/java/sv/edu/udb/guia07app/Activity_dashboard.java
@@ -0,0 +1,14 @@
+package sv.edu.udb.guia07app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
---
+++ Guia07App/app/src/main/java/sv/edu/udb/guia07app/MainActivity.java
@@ -0,0 +1,39 @@
+package sv.edu.udb.guia07app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button registerBtn, loginBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initializeViews();
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, activity_registration.class);
+                startActivity(intent);
+            }
+        });
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, activity_login.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initializeViews() {
+        registerBtn = findViewById(R.id.btnRegistrarse);
+        loginBtn = findViewById(R.id.btnLogin);
+    }
+}
